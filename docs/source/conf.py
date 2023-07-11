83a7aa299464fdda7951aab22b2e4657df23f6f2
Matt Jeff <jefcolbi@gmail.com>
2023-07-11 10:47:20 +0100

    update docs and migrations

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ copyright = '2021, MATTON Jef'
 author = 'MATTON Jef'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.2'
+release = '0.2.3'
 
 
 # -- General configuration ---------------------------------------------------
